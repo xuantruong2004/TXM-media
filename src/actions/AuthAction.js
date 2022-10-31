d1637c6b550530cacbbab49897cae97e2da134a7
xuantruong2004 <truongxuantp3@gmail.com>
2022-10-31 20:46:58 +0700

    update toastify

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -1,12 +1,15 @@
 import * as AuthApi from "../api/AuthRequest";
+import { toast } from "react-toastify";
 
 export const logIn = (formData) => async (dispatch) => {
   dispatch({ type: "AUTH_START" });
   try {
     const { data } = await AuthApi.logIn(formData);
     dispatch({ type: "AUTH_SUCCESS", data: data });
+    toast.success("login successfully ");
   } catch (error) {
     console.log(error.response.data);
+    toast.error(error.response.data);
     dispatch({ type: "AUTH_FAIL" });
   }
 };
@@ -16,12 +19,15 @@ export const signUp = (formData) => async (dispatch) => {
   try {
     const { data } = await AuthApi.signUp(formData);
     dispatch({ type: "AUTH_SUCCESS", data: data });
+    toast.success("sign up successfully ");
   } catch (error) {
     console.log(error);
+    toast.error(error.response.data);
     dispatch({ type: "AUTH_FAIL" });
   }
 };
 
 export const logout = () => async (dispatch) => {
+  toast.success("logout is successfully");
   dispatch({ type: "LOG_OUT" });
 };
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store/ReduxStore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <Provider store={store}>
+    <ToastContainer
+      theme="dark"
+      position="top-right"
+      autoClose={2000}
+      closeOnClick
+      pauseOnHover={false}
+    />
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<App />} />
